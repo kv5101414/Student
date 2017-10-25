6594b66bf14eb455294b7eac868578effc314141
kv5101414 <283002783@qq.com>
2017-10-25 09:33:04 +0800

    motified a thing

---
+++ A.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+public class A{
+   public static void main(String[] args){
+     /*
+        Scanner sc = new Scanner(System.in);
+               System.out.println("请输入小明的分数");
+             int score = sc.nextInt();
+          if(score<60 && score>=0){
+                          System.out.println("回家跪搓衣板");  
+          }else if(score>=60 && score<70){
+                          System.out.println("扫地一个礼拜");
+          }else if(score>=70 && score<80){
+                          System.out.println("三天不准出门");
+          }else if(score>=80 && score<90){
+                          System.out.println("允许大lol 2小时");
+          }else if(score>=90 && score<=100){
+                          System.out.println("出去旅游");
+          }else{
+                          System.out.println("请正确输入分数");
+          }   
+}
+
+           Scanner sc = new Scanner(System.in);
+           System.out.println("请输入一个年份");
+            int year = sc.nextInt();
+            if(year % 4 ==0 && year %100 !=0 || year %400 ==0){
+                  System.out.println("闰年");
+}           else{
+                   System.out.println("平年");
+}
+}
+
+
+String username ="admin";
+String password ="123456";
+
+Scanner sc = new Scanner(System.in);
+System.out.println("请输入用户名");
+String uname = sc.next();
+if(username.equals(uname)){
+
+       System.out.println("请输入密码");
+       String pw = sc.next();
+      if(password.equals(pw)){
+          System.out.println("欢迎登陆"); 
+}  else{
+         System.out.println("密码错误");
+        }    
+
+
+}else{
+     System.out.println("用户名错误");
+      }
+*/
+
+     
+     Scanner sc = new Scanner(System.in);
+
+     System.out.println("请输入字母");
+     String na = sc.next();
+     switch(){
+         case "a":
+             System.out.println(" 最强大便");
+            break;
+         case "b":
+              System.out.println("超凡大师");
+             break;
+         case "c":
+              System.out.println("璀璨钻石");
+             break;
+        default:
+              System.out.println("战5渣渣");
+             System.out.println("逗比")
+
+              }
+
+    }
+
+}
